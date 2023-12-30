c32c2776341e0cb87395cae7c363b9241c39d594
임수빈 <subinsutv@gmail.com>
2023-12-31 02:35:41 +0900

    🌶️::헤더 view 개발

---
+++ src/assets/imgs.ts
@@ -0,0 +1,1 @@
+export { default as defaultLogo } from "assets/imgs/defaultLogo.png";
---
+++ src/components/common/header/index.tsx
@@ -0,0 +1,67 @@
+import "styles/colorTheme.css";
+import styled from "@emotion/styled";
+import { defaultLogo } from "assets/imgs";
+import { useState } from "react";
+
+export default function Header() {
+  const [language, setLanguage] = useState(true);
+  const hendleChangeLanguage = () => {
+    setLanguage((pre) => !pre);
+  };
+  return (
+    <Container>
+      <Logo src={defaultLogo} />
+      <GNB>
+        <GNBText>결과</GNBText>
+        <GNBText>뿌이 유형 검사</GNBText>
+        <GNBText>뿌이 유형 모음</GNBText>
+        <GNBText>문의</GNBText>
+      </GNB>
+      <Language onClick={hendleChangeLanguage}>
+        {language ? <>🇰🇷 한국어</> : <>🇻🇳 뿌이어</>}
+      </Language>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  position: relative;
+  height: 80px;
+  padding: 0 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: var(--background-primary);
+`;
+
+const Logo = styled.img`
+  width: 210px;
+  cursor: pointer;
+`;
+
+const GNB = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const GNBText = styled.p`
+  color: var(--text-primary);
+  cursor: pointer;
+`;
+
+const Language = styled.button`
+  padding: 6px 16px;
+  border: none;
+  border-radius: 30px;
+  background-color: var(--background-primary);
+  transition: 0.4s;
+  &:hover {
+    background-color: var(--background-hover-primary);
+    cursor: pointer;
+  }
+`;
